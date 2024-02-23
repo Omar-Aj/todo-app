d5992e37a837e612ebc183bcc0adca1bd1572c60
Omar Ajaj <202110124@students.asu.edu.jo>
2024-02-23 23:02:46 +0300

    init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primary-dark": "#525252",
       },
     },
   },
