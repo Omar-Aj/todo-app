8e3bd7a5a66f0379307d873a82451a0867edda5e
Omar Ajaj <omar.aj.business@gmail.com>
2024-09-28 11:42:20 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/todo-app",
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
